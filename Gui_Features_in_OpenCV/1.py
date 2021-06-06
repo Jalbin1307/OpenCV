5b5d34c4b62ce471ed3433831120e640d0bbf7df
Jalbin1307 <az13077@gmail.com>
2021-06-06 20:02:00 +0900

    docs : upload 1.py and img file

---
+++ Gui_Features_in_OpenCV/1.py
@@ -0,0 +1,15 @@
+import cv2 as cv
+import sys
+
+
+img = cv.imread(r'C:\Users\USER\OpenCV\OpenCV\Gui_Features_in_OpenCV\starry_night.jpg')
+
+if img is None:
+    sys.exit("Could not read the image.")
+
+cv.imshow("Display window", img)
+
+k = cv.waitKey(0)
+
+if k == ord("s"):
+    cv.imwrite("starry_night.png", img)
